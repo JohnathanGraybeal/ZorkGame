00c585cf463fe90bd7a9a265dfa4c96ee840413a
Johnathan Graybeal <graybealjohnathan@gmail.com>
2019-06-12 20:25:45 -0400

    Player Race Enumeration

---
+++ Player/PlayerRace.java
@@ -0,0 +1,18 @@
+/*
+ * ---------------------------------------------------------------------------
+ * File name: PlayerRace.java
+ * Project name: ZorkGame
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Johnathan Graybeal, graybealjw@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: 4/10/2019
+ * ---------------------------------------------------------------------------
+ */
+
+package Player;
+
+public enum PlayerRace
+{
+	HUMAN,ELF,
+	GOLIATH,DWARF
+}
