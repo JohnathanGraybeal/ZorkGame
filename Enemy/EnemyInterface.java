9caf839854429093e2e77d31648956274687edd6
Johnathan Graybeal <graybealjohnathan@gmail.com>
2019-06-12 20:28:10 -0400

    Enemy Interface

---
+++ Enemy/EnemyInterface.java
@@ -0,0 +1,23 @@
+/*
+ * ---------------------------------------------------------------------------
+ * File name: EnemyInterface.java
+ * Project name: ZorkGame
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Johnathan Graybeal, graybealjw@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: 4/10/2019
+ * ---------------------------------------------------------------------------
+ */
+
+package Enemy;
+
+public interface EnemyInterface
+{
+	 void setName();
+
+	void stats();
+
+
+
+
+}
